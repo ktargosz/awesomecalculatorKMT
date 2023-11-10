df58f408157617d3b87720e834c9041628aaacca
kenta <kenta@RAZER15A20>
2023-11-10 16:57:35 -0500

    updated username and github

--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
+    name="awesomecalculatorKMT", # Replace with your own username
     version= "1.0.0",
     author="Daniel Edreira",
     author_email="dedreira@github.com",
     description="An awesome calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dedreira/awesomecalculator",
+    url="https://github.com/ktargosz/awesomecalculatorKMT",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
